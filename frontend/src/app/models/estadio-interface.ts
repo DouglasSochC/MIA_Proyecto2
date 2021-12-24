147a4bc765a99e0a24492b0682d5d6b63081184b
agarcia17948 <2321399700101@ingenieria.usac.edu.gt>
2021-12-24 16:13:44 -0600

    mostrando strings de  pais y estado

--- frontend/src/app/models/estadio-interface.ts
+++ frontend/src/app/models/estadio-interface.ts
@@ -1,15 +1,36 @@
 
 
+/*
+
+            "id_estadio": registro[0],
+            "estadio_nombre": registro[1],
+            "fecha_inaguracion": registro[2],
+            "capacidad": registro[3],
+            "direccion": registro[4],
+            "link_fotografia": registro[5],
+            
+            "id_estado_estadio": registro[6],
+            "estado_nombre": registro[7],
+            "id_pais": registro[8],
+            "pais_nombre": registro[9]
+
+
+*/
+
+
 export interface EstadioInterface{
     id_estadio:number,
     estadio_nombre:string, 
     fecha_inaguracion: string,
     capacidad:number, 
-    id_pais:number, 
     direccion:string,
+    link_fotografia:string,
     id_estado_estadio:number,
-    link_fotografia:string
+    estado_nombre:string,
+    id_pais:number,
+    pais_nombre:string    
 }
+
 
 export interface PaisInterface{
     id_pais: number,
